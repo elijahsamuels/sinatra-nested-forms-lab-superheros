a475fe862d2e5b57cfbf60e01995f08e7fc0eab4
elijahsamuels <elijahsamuels@gmail.com>
2020-12-23 18:09:44 -0700

    commit tree c1874030d5341e8d4811e747a7389bac95603717

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/teams' do
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @hero_name = []
+        @hero_power = []
+        @hero_bio = []
+        @team_members = params[:team][:members]
+        @team_members.each do |i|
+            # binding.pry
+            @hero_name << i[:name]
+            @hero_power << i[:power]
+            @hero_bio << i[:bio]
+            end
+            erb :team
+        end        
 
 end
